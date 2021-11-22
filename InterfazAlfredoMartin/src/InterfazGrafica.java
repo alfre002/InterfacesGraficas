4eddb6348fd345824da46a4f0004c27c9bb24acf
alfre002 <amarrui2808@iesmartinezm.es>
2021-11-22 09:28:47 +0100

    1er commit: importe de proyecto

---
+++ InterfazAlfredoMartin/src/InterfazGrafica.java
@@ -0,0 +1,62 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class InterfazGrafica {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfazGrafica window = new InterfazGrafica();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public InterfazGrafica() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblNewLabel = new JLabel("Alfredo Martin");
+		frame.getContentPane().add(lblNewLabel, BorderLayout.CENTER);
+		
+		textField = new JTextField();
+		frame.getContentPane().add(textField, BorderLayout.EAST);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Enviar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		frame.getContentPane().add(btnNewButton, BorderLayout.SOUTH);
+	}
+
+}
